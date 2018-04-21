7b020a7877fcb3884531ed3cd731604c51f98da8
Daniel Wright <dwwright@alumni.iastate.edu>
2018-04-21 17:05:02 -0500

    Added some logging messages

--- family-tree-maker-desktop/src/main/java/com/wright/ftm/GuiMain.java
+++ family-tree-maker-desktop/src/main/java/com/wright/ftm/GuiMain.java
@@ -6,18 +6,26 @@ import com.wright.ftm.stages.FatalAlert;
 import com.wright.ftm.stages.StageUtils;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GuiMain extends Application {
+    private static Logger logger;
     private DbManager dbManager = DbManager.getInstance();
 
     public static void main(String[] args) {
         LoggerConfigurationService loggerConfigurationService = new LoggerConfigurationService();
         loggerConfigurationService.configure();
 
+        getLogger().info("**********************************************");
+        getLogger().info("********* Family Tree Maker Desktop **********");
+        getLogger().info("**********************************************");
+
         launch(args);
     }
 
     public void start(Stage primaryStage) throws Exception {
+        getLogger().info("Starting app...");
         boolean databaseSuccessfullyStarted = dbManager.startDb();
 
         primaryStage.setHeight(700);
@@ -31,12 +39,22 @@ public class GuiMain extends Application {
         if (!databaseSuccessfullyStarted) {
             FatalAlert.show(primaryStage, "Failed to load data!");
         }
+        getLogger().info("App startup");
     }
 
-    @Override
     public void stop() throws Exception {
+        getLogger().info("Stopping app...");
         dbManager.stopDb();
 
         super.stop();
+        getLogger().info("App stopped");
+    }
+
+    private static Logger getLogger() {
+        if (logger == null) {
+            logger = LoggerFactory.getLogger(GuiMain.class);
+        }
+
+        return logger;
     }
 }
--- family-tree-maker-desktop/src/main/java/com/wright/ftm/services/DbConfigurationService.java
+++ family-tree-maker-desktop/src/main/java/com/wright/ftm/services/DbConfigurationService.java
@@ -2,6 +2,8 @@ package com.wright.ftm.services;
 
 import com.wright.ftm.db.DbManager;
 import com.wright.ftm.repositories.TablesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -9,35 +11,45 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class DbConfigurationService {
+    private final Logger logger = LoggerFactory.getLogger(DbConfigurationService.class);
     private Connection connection = DbManager.getInstance().getConnection();
     private JarFileReaderService jarFileReaderService = new JarFileReaderService();
     private TablesRepository tablesRepository = new TablesRepository(connection);
 
     public boolean configure() {
         try {
+            logger.info("Configuring database...");
             List<String> tableNames = tablesRepository.query();
             if (tableNames.contains("DB_VERSION")) {
                 executeStatement("SET SCHEMA FAMILY_TREE_MAKER");
             } else {
                 runInitialConfiguration();
             }
+            logger.info("Database configuration complete");
 
             return true;
         } catch (SQLException | IOException e) {
+            logger.error("Error while configuring database: " + e.getMessage());
             return false;
         }
     }
 
     private void runInitialConfiguration() throws IOException, SQLException {
+        logger.info("Running initial configuration...");
         executeScript("/scripts/db/initial-schema-000001.sql");
+        logger.info("Initial configuration complete");
     }
 
     private void executeScript(String scriptJarPath) throws IOException, SQLException {
+        logger.info("Executing configuration script: " + scriptJarPath + "...");
+
         String scriptContents = jarFileReaderService.readAsString(scriptJarPath);
         String[] statements = scriptContents.trim().split(";");
         for (String statement : statements) {
             executeStatement(statement.trim());
         }
+
+        logger.info("Script execution complete");
     }
 
     private void executeStatement(String statementString) throws SQLException {
