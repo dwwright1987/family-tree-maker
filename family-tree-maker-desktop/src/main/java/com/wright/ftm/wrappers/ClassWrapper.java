0f4a2747bd07d646c6e0adaa4ffe06570b7c3f2d
Daniel Wright <dwwright@alumni.iastate.edu>
2018-04-22 11:44:09 -0500

    Java 9

--- family-tree-maker-desktop/src/main/java/com/wright/ftm/wrappers/ClassWrapper.java
+++ family-tree-maker-desktop/src/main/java/com/wright/ftm/wrappers/ClassWrapper.java
@@ -1,9 +1,6 @@
 package com.wright.ftm.wrappers;
 
-import sun.reflect.CallerSensitive;
-
 public class ClassWrapper {
-    @CallerSensitive
     public Object forName(String className) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
         return Class.forName(className).newInstance();
     }
--- family-tree-maker-desktop/src/main/java/com/wright/ftm/wrappers/DriverManagerWrapper.java
+++ family-tree-maker-desktop/src/main/java/com/wright/ftm/wrappers/DriverManagerWrapper.java
@@ -1,13 +1,10 @@
 package com.wright.ftm.wrappers;
 
-import sun.reflect.CallerSensitive;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverManagerWrapper {
-    @CallerSensitive
     public Connection getConnection(String url) throws SQLException {
         return DriverManager.getConnection(url);
     }
