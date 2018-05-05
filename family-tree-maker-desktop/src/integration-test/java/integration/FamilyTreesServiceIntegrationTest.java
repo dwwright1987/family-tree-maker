b77341d00b86e569f35d5c181096834b042e71a4
Daniel Wright <dwwright@alumni.iastate.edu>
2018-05-05 17:02:22 -0500

    Wrote test to write and read family trees

--- family-tree-maker-desktop/src/integration-test/java/integration/FamilyTreesServiceIntegrationTest.java
+++ family-tree-maker-desktop/src/integration-test/java/integration/FamilyTreesServiceIntegrationTest.java
@@ -1,14 +1,19 @@
 package integration;
 
 import com.wright.ftm.db.DbManager;
+import com.wright.ftm.dtos.FamilyTreeDTO;
+import com.wright.ftm.services.FamilyTreesService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FamilyTreesServiceIntegrationTest {
     private DbManager dbManager = DbManager.getInstance();
+    private FamilyTreesService familyTreesService = new FamilyTreesService();
 
     @BeforeEach
     void setUp() throws Exception {
@@ -22,7 +27,14 @@ public class FamilyTreesServiceIntegrationTest {
     }
 
     @Test
-    void test1() throws Exception {
-        assertEquals(1, 1);
+    void testWritesAndReadsFamilyTrees() throws Exception {
+        String expectedFamilyName = "Wright";
+
+        familyTreesService.createFamilyTree(expectedFamilyName);
+
+        List<FamilyTreeDTO> allFamilyTrees = familyTreesService.getAllFamilyTrees();
+
+        assertEquals(1, allFamilyTrees.size());
+        assertEquals(expectedFamilyName, allFamilyTrees.get(0).getName());
     }
 }
